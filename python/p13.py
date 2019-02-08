706a3481e9312c22504bf0296bd55eea2ede17cd
0 <41608732+AntonTerehovPerm@users.noreply.github.com>
2019-02-08 10:59:16 +0300

    Ошибки и красота проги

--- python/p13.py
+++ python/p13.py
@@ -1,9 +1,9 @@
 """Определить существование треугольника и его тип"""
-a = float(input('vedita 1 storonu, zatem 2, zatem 3'))
+a = float(input('vedita 1 storonu, zatem 2, zatem 3')) #если бы кто-то пользовался этим, ему было бы неудобно, исправь вывод информации на экран
 b = float(input())
 c = float(input())
 if a == b == c :
     print('равносторонний')
-elif a !=b and b!=c and c!=a:
+elif a !=b and b!=c and (c!=a): #если ты разделяешь или связываешь условия, они должны быть в скобках, все!
     print('разносторонний')
 else: print('равнобедренный')
